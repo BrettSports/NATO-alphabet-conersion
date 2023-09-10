8d9ae9ead101ad7d600ff53e8339ff5caf4be2f4
Brett Sports <omnipotentshoemaker@gmail.com>
2023-09-10 01:43:57 -0400

    Updated code to address TypeError

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ import pandas as pd
 nato_data_frame = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for index, row in nato_data_frame.iterrows()}
 
+def generate_phonetic():
+    user_word = input("Enter a word... if you feel comfortable doing so: ")
+    try:
+        user_list = [nato_dict[char.upper()] for char in user_word]
+    except KeyError:
+        print("Please print only words.")
+        generate_phonetic()
+    else:
+        print(user_list)
 
-user_word = input("Enter a word... if you feel comfortable doing so: ")
-user_list = [nato_dict[char.upper()] for char in user_word]
-print(user_list)
+generate_phonetic()
